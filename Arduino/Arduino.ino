1743c39feaff9467fb74a9ab3fb21b343c829ccb
dereisl <elias.nt543@gmail.com>
2018-11-14 10:36:15 +0100

    Add Code

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -1,9 +1,30 @@
+#include <FastLED.h>
+
+#define LED_COUNT 8
+#define LED_PIN 2
+
+CRGB leds[LED_COUNT];
+
 void setup() {
-  // put your setup code here, to run once:
-  
+  FastLED.addLeds<NEOPIXEL, LED_PIN>(leds, LED_COUNT);
+}
+void loop() {
+  for (int i = 0; i < LED_COUNT; i++) {
+    turnOnAmount(i);
+    delay(30);
+  }
+  for (int i = LED_COUNT - 1; i >= 0; i--) {
+    turnOnAmount(i);
+    delay(30);
+  }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  
+void turnOnAmount(int amount) {
+  for (int i = 0; i < LED_COUNT; i++) {
+    leds[i] = CRGB::Black;
+  }
+  for (int i = 0; i <= amount; i++) {
+    leds[i] = CHSV(170 - (i * (170 / LED_COUNT + 1)), 255, 100);
+  }
+  FastLED.show();
 }
