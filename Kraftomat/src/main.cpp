5a28b309a70d0d6fa477be1ea5d980b73d50630f
dereisl <32849066+dereisl@users.noreply.github.com>
2018-11-14 15:27:14 +0100

    Document code

--- Kraftomat/src/main.cpp
+++ Kraftomat/src/main.cpp
@@ -4,50 +4,58 @@
 #define LED_PIN 2
 
 #define STRENGTH_PIN A0
+#define STRENGTH_MIN 50
 #define STRENGTH_MAX 400
 
-int avalue, i, avalues[50];
+int avalue, avalues[50];
 CRGB leds[LED_COUNT];
 
 void strengthAnimation(int value);
 void turnOnAmount(int amount);
 
 void setup() {
+  // Initialize WS2812B library
   FastLED.addLeds<NEOPIXEL, LED_PIN>(leds, LED_COUNT);
-  // Serial.begin(9600);
 }
 
 void loop() {
-  while(avalue < 50){
+  // Treshold for triggering LEDs
+  while(avalue < STRENGTH_MIN){
     avalue = analogRead(STRENGTH_PIN);
   }
+  // Meassure 50 times
   for(int i = 0; i < 50; i++){
     avalues[i] = analogRead(STRENGTH_PIN);
   }
+  // Take the highest value of 50 meassuring points
   for(int i = 0; i < 50; i++){
     if(avalues[i] > avalue){
       avalue = avalues[i];
     }
   }
-  // Serial.print(avalue);
+  // Start LED animation
   strengthAnimation(avalue);
+  // Reset avalue
   avalue = 0;
 }
 
 void strengthAnimation(int value) {
+  // Calculate the amount of LEDs to light up
   int leds = (value * LED_COUNT) / STRENGTH_MAX;
-  // Serial.println(leds);
+  // Animate build up
   for (int i = 1; i <= leds; i++) {
     turnOnAmount(i);
     delay(30);
   }
   delay(500);
+  // Animate down again
   for (int i = leds; i >= 0; i--) {
     turnOnAmount(i);
     delay(30);
   }
 }
 
+// Function to turn off a specific amount of LEDs
 void turnOnAmount(int amount) {
   for (int i = 0; i < LED_COUNT; i++) {
     leds[i] = CRGB::Black;
